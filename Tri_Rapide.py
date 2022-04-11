b77ea7fe38b5b1f020270e09a8350d9d4ee4ff65
ClaraDel <clara.ferru@epfedu.fr>
2022-04-10 22:10:28 -0400

    algorithme tri rapide terminé

--- Tri_Rapide.py
+++ Tri_Rapide.py
@@ -1,4 +1,4 @@
-import random
+import random as r
 
 def separation(liste, pivot, i):
     """Entrée : une liste, un pivot et la place du pivot dans la liste
@@ -19,12 +19,12 @@ def quicksort(liste):
     """Entrée : une liste
        Sortie : une liste avec les mêmes éléments triés par l'algorithme de tri rapide randomisé"""
     n = len(liste)
-    if n == 1:
+    if n == 1 or n==0:
         """Une liste à 1 élément est toujours triée"""
         return liste
     else:
         """Choix du pivot AU HASARD dans la liste"""
-        i = randint(0, n - 1)
+        i = r.randint(0, n - 1)
         pivot = liste[i]
 
         """On sépare en 2 listes de taille strictement plus petite que n car le pivot n'appartient à aucune des deux listes"""
